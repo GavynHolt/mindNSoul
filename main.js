95e4e43564b1fa8cd37383a4c27be207431752c6
Gavyn Holt <gavynholt@gmail.com>
2021-07-06 19:01:04 -0400

    completed Modal on main page

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // open and close hamburger menu
   document.querySelector(".hamburger").addEventListener("click", function () {
     console.log("hamburger clicked");
     const navMenu = document.querySelector("#navLinks");
@@ -8,4 +9,30 @@ document.addEventListener("DOMContentLoaded", function () {
       navMenu.style.height = "auto";
     }
   });
+
+  // open modal
+  const openButtonList = document.querySelectorAll(".openModal");
+
+  for (button of openButtonList) {
+    button.addEventListener("click", function (e) {
+      const currentModal =
+        e.target.parentNode.parentNode.querySelector(".modalRoot");
+      currentModal.classList.add("show");
+      // event listener to close modal
+      const closeModalButton = currentModal.querySelector(".closeModal");
+      closeModalButton.addEventListener("click", function () {
+        currentModal.classList.remove("show");
+      });
+      // ESC key close modal (once: true, ensures listener is turned off after use)
+      document.addEventListener(
+        "keydown",
+        function (e) {
+          if (e.key.toLowerCase() == "escape") {
+            currentModal.classList.remove("show");
+          }
+        },
+        { once: true }
+      );
+    });
+  }
 });
