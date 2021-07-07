519d9223cab0d12a877ded52a77e470aa79586b9
Gavyn Holt <gavynholt@gmail.com>
2021-07-06 23:30:46 -0400

    refined modal event listeners. 3 event listeners are now removed on modal close

--- main.js
+++ main.js
@@ -15,28 +15,47 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // open modal
-  // add event listeners to all buttons
+  // add event listeners to all buttons for 3 close events
+  // remove all close event listers on modal close
   const openButtonList = document.querySelectorAll(".openModal");
   for (button of openButtonList) {
     button.addEventListener("click", function (e) {
+      // get modal and close button objects
       const currentModal =
         e.target.parentNode.parentNode.querySelector(".modalRoot");
-      currentModal.classList.add("show");
-      // event listener to close modal
       const closeModalButton = currentModal.querySelector(".closeModal");
-      closeModalButton.addEventListener("click", function () {
+      // show modal
+      currentModal.classList.add("show");
+
+      // close modal and remove close event listeners
+      const closeModal = function () {
         currentModal.classList.remove("show");
-      });
+        currentModal.removeEventListener("click", clickOffModalToClose);
+        closeModalButton.removeEventListener("click", closeModal);
+        document.removeEventListener("keydown", ESCKeyModalClose);
+      };
+      // check for clicks outside modal, close modal
+      const clickOffModalToClose = function (e) {
+        if (
+          e.target.closest(".modal") === null &&
+          e.target.closest(".openModal") === null
+        ) {
+          closeModal();
+        }
+      };
+      // close modal on ESC keypress
+      const ESCKeyModalClose = function (e) {
+        if (e.key.toLowerCase() == "escape") {
+          closeModal();
+        }
+      };
+
+      // event listener for mouse clicks off modal to close modal
+      currentModal.addEventListener("click", clickOffModalToClose);
+      // event listener to close modal on close button click
+      closeModalButton.addEventListener("click", closeModal);
       // ESC key close modal (once: true, ensures listener is turned off after use)
-      document.addEventListener(
-        "keydown",
-        function (e) {
-          if (e.key.toLowerCase() == "escape") {
-            currentModal.classList.remove("show");
-          }
-        },
-        { once: true }
-      );
+      document.addEventListener("keydown", ESCKeyModalClose);
     });
   }
 });
